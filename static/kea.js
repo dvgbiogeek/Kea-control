d08358433123ce1e7bb5cd28b1debb5299faf920
glickdv <dvgbiogeek@gmail.com>
2015-06-26 15:38:15 -0700

    updates to kea controller and routing for resume

--- static/kea.js
+++ static/kea.js
@@ -1,19 +1,45 @@
-angular.module('KeaApp', [])
-  .controller('KeaController', ['$scope', function($scope) {
+angular.module('KeaApp', ['ngRoute'])
+  .config(['$routeProvider', '$locationProvider', 
+    function($routeProvider, $locationProvider) {
+
+      $routeProvider.when('/', {
+        templateUrl: '/static/kea.html',
+        controller: 'KeaController'
+      })
+
+      .when('/resume', {
+        templateUrl: '/static/resume.html'
+      })
+
+      .otherwise({redirectTo: ('/')});
+  }])
+
+  .controller('KeaController', ['KeaService', function(KeaService) {
+    var self = this;
+    self.getQuote = KeaService.getQuote();
     
-    $scope.quotes = [
-      {label: "hey! gimme food!"},
+    this.newQuote = function() {
+      self.getQuote = KeaService.getQuote();
+    };
+  }])
+
+  .factory('KeaService', [function() {
+    var quotes = [
+      {label: "Hey! gimme food!"},
       {label: "I'll peck your tires"},
       {label: "I'll destroy your car"},
       {label: "You want to take me home?"},
       {label: "People food is delicious"},
       {label: "Aren't I cute?"},
       {label: "Stupid human"},
+      {label: "Define bird brain for me"},
+      {label: "What are you looking at?"}
     ];
 
-    $scope.randomQuote = $scope.quotes[Math.floor(Math.random() * $scope.quotes.length)];
-    
-    $scope.newQuote = function() {
-      $scope.randomQuote = $scope.quotes[Math.floor(Math.random() * $scope.quotes.length)];
+    return {
+      getQuote: function() {
+        return quotes[Math.floor(Math.random() * quotes.length)];
+      }
     };
   }]);
+
